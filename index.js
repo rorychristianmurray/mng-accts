96e30d9224db185acb07f0f322be6482ea40461f
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2021-12-19 11:10:15 -0500

    Get account info

--- index.js
+++ index.js
@@ -17,18 +17,24 @@ const apiConfig = new Configuration({
 });
 
 const privateKey = makeRandomPrivKey();
-console.log("privateKey : ", privateKey);
+// console.log("privateKey : ", privateKey);
 
-const stacksAddress = getAddressFromPrivateKey(
-  privateKeyToString(privateKey),
-  TransactionVersion.Testnet // remove for mainnet
-);
+// const stacksAddress = getAddressFromPrivateKey(
+//   privateKeyToString(privateKey),
+//   TransactionVersion.Testnet // remove for mainnet
+// );
+
+// faucet address
+// const stacksAddress = "ST2QKZ4FKHAH1NQKYKYAYZPY440FEPK7GZ1R5HBP2";
+
+const stacksAddress = "ST1P8Q8JGPHXB7PVQV42QP962KGV5P77JP6MT8DJ9";
 
 const accounts = new AccountsApi(apiConfig);
 
 async function getAccountInfo() {
   const accountInfo = await accounts.getAccountInfo({
     principal: stacksAddress,
+    proof: 0,
   });
 
   return accountInfo;
@@ -39,4 +45,42 @@ const getInfo = async () => {
   console.log("info : ", info);
 };
 
+async function runFaucetStx() {
+  const faucets = new FaucetsApi(apiConfig);
+
+  const faucetTx = await faucets.runFaucetStx({
+    address: stacksAddress,
+  });
+  console.log("faucetTx : ", faucetTx);
+
+  return faucetTx;
+}
+
+// runFaucetStx();
 getInfo();
+
+// https://explorer.stacks.co/txid/0x48b9fb83e66fcd74e513cbb877d94408259d789cdda098dc76f7b06ddb391f53
+
+async function getAccountTransactions() {
+  const history = await accounts.getAccountTransactions({
+    principal: stacksAddress,
+  });
+  console.log("history : ", history);
+
+  return history;
+}
+
+getAccountTransactions();
+
+async function getAccountBalances() {
+  const balances = await accounts.getAccountBalance({
+    principal: stacksAddress,
+  });
+
+  // The balance field is denoted in micro-STX units. 1,000,000 micro-STX are worth 1 Stacks (STX) token.
+  console.log("balance : ", balances);
+
+  return balances;
+}
+
+getAccountBalances();
